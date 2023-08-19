5b5c5cc680dfe14133a82826518b728f9081caf8
wendigo295 <75575185+wendigo295@users.noreply.github.com>
2023-08-19 21:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from torch import cuda
 from langchain.text_splitter import CharacterTextSplitter
